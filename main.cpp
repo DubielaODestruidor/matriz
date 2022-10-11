cc7382d5e404b5f4750166b4fae4374b73ee19d9
João Dubiela <joaopedro.dubiela@hotmail.com>
2022-10-11 19:08:42 -0300

    Remoção de ruído de imagem

--- main.cpp
+++ main.cpp
@@ -4,25 +4,92 @@
 using namespace mat;
 using namespace std;
 
-int main()
-{
-    auto x = Matrix<int>(5, 5);
-    auto y = Matrix<int>(5, 5);
+int main() {
 
-    for(int i = 0; i < x.get_num_rows(); i++)
+    ifstream arq("001.pgm");
+    //arg >> t >> w >> h >> m;
+    //ifstream arq("teste.pgm");
+    string sLine;
+    string header, dimentions, max_input;
+    getline(arq, sLine);
+    header = sLine;
+    getline(arq, sLine);
+    dimentions = sLine;
+    getline(arq, sLine);
+    max_input = sLine;
+    string dimentionsx = dimentions.substr(0, dimentions.find(" "));
+    string dimentionsy = dimentions.substr(dimentions.find(" ") + 1, dimentions.size());
+    int y = atoi(dimentionsx.c_str());
+    int x = atoi(dimentionsy.c_str());
+//    cout << header << endl;
+//    cout << x << " " << y << endl;
+//    cout << max_input << endl;
+    int vetPos = 0;
+    int vet[1000000];
+
+    int cont = 0;
+    /*while (!arq.eof()) {
+        getline(arq, sLine);
+        int count2 = 0;
+        for (int i = 0; i < sLine.size(); i++) {
+            if (sLine[i] == ' ') {}
+            else {
+                const char *c = &sLine[i];
+                int n = atoi(c);
+                cout << n << endl;
+                vet[vetPos] = n;
+                vetPos++;
+                i++;
+                count2++;
+            }
+        }
+        cont++;
+        arq.close();
+    }*/
+
+    string s;
+    int k = 0;
+    while (getline(arq, s))
     {
-        for(int j = 0; j < x.get_num_cols(); j++)
+        istringstream line(s);
+        string s2;
+
+        int l = 0;
+        while (getline(line, s2, ' '))
         {
-            x.at(i, j) = i + j;
-            y.at(i, j) = i - j;
+            int n = stoi(s2);
+            vet[k] = n;
+            ++k;
+            ++l;
+            //cout << l << endl;
         }
     }
 
-    auto z = x * y;
+    auto matriz = Matrix<int>(x, y);
 
-    std::cout << x.serialize() << "\n";
-    std::cout << y.serialize() << "\n";
-    std::cout << z.serialize() << "\n";
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            //cout << vet[i * y + j] << " ";
+            //cout << vet[90000] << " ";
+            matriz.at(i, j) = vet[i * y + j];
+        }
+    }
+
+//    for (int i = 0; i < 1000; ++i)
+//    {
+//        cout << vet[i] << "\t";
+//    }
+    //cout << matriz.serialize() << endl;
+    //cout << matriz.salt_and_pepper_removal().serialize();
+    //cout << matriz.noise_removal().serialize();
+
+    ofstream outfile ("image.txt");
+    outfile << "P2" << endl << y << " " << x << endl << max_input << endl;
+    //auto x1 = matriz.noise_removal();
+    auto x1 = matriz.noise_removal_median();
+    outfile << x1.serialize();
+
+    outfile.close();
 
     return 0;
 }
--- matrix.h
+++ matrix.h
@@ -2,63 +2,55 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <thread>
+#include <iostream>
+#include <algorithm>
 
 namespace mat
 {
     template <typename T = double>
-    class Matrix
-    {
-        std::vector< std::vector<T> > data;
+    class Matrix {
+        std::vector<std::vector<T> > data;
 
     public:
 
         // Constroi uma matriz vazia
-        Matrix()
-        {
+        Matrix() {
         }
 
         // Constroi matriz m*n
-        Matrix(int m, int n)
-        {
+        Matrix(int m, int n) {
             set_size(m, n);
         }
 
         // Acessa elemento da matriz
-        T & at(int i, int j)
-        {
+        T &at(int i, int j) {
             return data.at(i).at(j);
         }
 
         // Pega número de linhas
-        int get_num_rows() const
-        {
+        int get_num_rows() const {
             return data.size();
         }
 
         // Pega número de colunas
-        int get_num_cols()
-        {
+        int get_num_cols() {
             return data.size() ? data.at(0).size() : 0;
         }
 
         // Define tamanho da matriz
-        void set_size(int m, int n)
-        {
+        void set_size(int m, int n) {
             data.resize(m);
-            for(auto & row : data)
-            {
+            for (auto &row: data) {
                 row.resize(n);
             }
         }
 
         // Serializa matriz para impressão
-        std::string serialize(std::string const & separator = " ")
-        {
+        std::string serialize(std::string const &separator = " ") {
             std::ostringstream output;
-            for(auto & row : data)
-            {
-                for(auto & item : row)
-                {
+            for (auto &row: data) {
+                for (auto &item: row) {
                     output << item << separator;
                 }
                 output << "\n";
@@ -67,31 +59,24 @@ namespace mat
         }
 
         // Zera os elementos da matriz
-        void zero()
-        {
-            for(auto & row : data)
-            {
-                for(auto & item : row)
-                {
+        void zero() {
+            for (auto &row: data) {
+                for (auto &item: row) {
                     item = T();
                 }
             }
         }
 
         // Soma matrizes
-        Matrix<T> operator+(Matrix<T> & other)
-        {
+        Matrix<T> operator+(Matrix<T> &other) {
             auto const m = get_num_rows();
             auto const n = get_num_cols();
-            if(m != other.get_num_rows() || n != other.get_num_cols())
-            {
+            if (m != other.get_num_rows() || n != other.get_num_cols()) {
                 throw std::runtime_error("cannot sum matrices with different sizes");
             }
             auto sum = Matrix<T>(m, n);
-            for(int i = 0; i < m; i++)
-            {
-                for(int j = 0; j < n; j++)
-                {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
                     sum.at(i, j) = data.at(i).at(j) + other.at(i, j);
                 }
             }
@@ -99,19 +84,15 @@ namespace mat
         }
 
         // Subtrai matrizes
-        Matrix<T> operator-(Matrix<T> & other)
-        {
+        Matrix<T> operator-(Matrix<T> &other) {
             auto const m = get_num_rows();
             auto const n = get_num_cols();
-            if(m != other.get_num_rows() || n != other.get_num_cols())
-            {
+            if (m != other.get_num_rows() || n != other.get_num_cols()) {
                 throw std::runtime_error("cannot subtract matrices with different sizes");
             }
             auto difference = Matrix<T>(m, n);
-            for(int i = 0; i < m; i++)
-            {
-                for(int j = 0; j < n; j++)
-                {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
                     difference.at(i, j) = data.at(i).at(j) - other.at(i, j);
                 }
             }
@@ -119,37 +100,30 @@ namespace mat
         }
 
         // Retorna matriz negada
-        Matrix<T> operator-()
-        {
+        Matrix<T> operator-() {
             auto const m = get_num_rows();
             auto const n = get_num_cols();
             auto negated = Matrix<T>(m, n);
-            for(int i = 0; i < m; i++)
-            {
-                for(int j = 0; j < n; j++)
-                {
-                    negated.at(i).at(j) = - data.at(i).at(j);
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    negated.at(i).at(j) = -data.at(i).at(j);
                 }
             }
             return negated;
         }
 
         // Retorna cópia da matriz
-        Matrix<T> operator+()
-        {
+        Matrix<T> operator+() {
             return *this;
         }
 
         // Multiplica por escalar
-        Matrix<T> operator*(T value)
-        {
+        Matrix<T> operator*(T value) {
             auto const m = get_num_rows();
             auto const n = get_num_cols();
             auto product = Matrix<T>(m, n);
-            for(int i = 0; i < m; i++)
-            {
-                for(int j = 0; j < n; j++)
-                {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
                     product.at(i, j) = data.at(i).at(j) * value;
                 }
             }
@@ -157,22 +131,17 @@ namespace mat
         }
 
         // Multiplicar matrizes
-        Matrix<T> operator*(Matrix<T> & other)
-        {
+        Matrix<T> operator*(Matrix<T> &other) {
             auto const m = get_num_rows();
             auto const n = get_num_cols();
             auto const p = other.get_num_cols();
-            if(n != other.get_num_rows())
-            {
+            if (n != other.get_num_rows()) {
                 throw std::runtime_error("Erro no número de colunas/linhas");
             }
             auto product = Matrix<T>(m, p);
-            for(int i = 0; i < m; i++)
-            {
-                for(int j = 0; j < p; j++)
-                {
-                    for(int k = 0; k < n; k++)
-                    {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < p; j++) {
+                    for (int k = 0; k < n; k++) {
                         product.at(i, j) += data.at(i).at(k) * other.at(k, j);
                     }
                 }
@@ -180,32 +149,68 @@ namespace mat
             return product;
         }
 
-        // Convolução da matriz com outro kernel
-        Matrix<T> convolve(Matrix<T> & kernel)
+        //Tirar ruído sal e pimenta com média
+        Matrix<T> noise_removal()
         {
             auto const m = get_num_rows();
             auto const n = get_num_cols();
-            auto const p = kernel.get_num_rows();
-            auto const q = kernel.get_num_cols();
-            auto const r = (p - 1) / 2;
-            auto const s = (q - 1) / 2;
             auto convolved = Matrix<T>(m, n);
-            for(int i = 0; i < m; i++)
-            {
-                for(int j = 0; j < n; j++)
-                {
-                    for(int k = 0; k < p; k++)
+            convolved.zero();
+            for (int i = 1; i < m - 1; i++) {
+                for (int j = 1; j < n - 1; j++) {
+                    int values[8];
+                    values[0] = data.at(i - 1).at(j - 1);
+                    values[1] = data.at(i - 1).at(j + 0);
+                    values[2] = data.at(i - 1).at(j + 1);
+
+                    values[3] = data.at(i).at(j - 1);
+                    values[4] = data.at(i).at(j + 1);
+
+                    values[5] = data.at(i + 1).at(j - 1);
+                    values[6] = data.at(i + 1).at(j + 0);
+                    values[7] = data.at(i + 1).at(j + 1);
+
+                    int average = 0;
+                    for (int k = 0; k < 8; k++)
                     {
-                        for(int l = 0; l < q; l++)
-                        {
-                            auto const x = i + k - r;
-                            auto const y = j + l - s;
-                            if(x >= 0 && x < m && y >= 0 && y < n)
-                            {
-                                convolved.at(i, j) += data.at(x).at(y) * kernel.at(k, l);
-                            }
-                        }
+                        average += values[k];
                     }
+                    average = (int)((float)average / 8.0f);
+
+                    convolved.at(i, j) = average;
+                }
+            }
+            return convolved;
+        }
+        
+        //Tirarndo ruído sal e pimenta com mediana
+        Matrix<T> noise_removal_median()
+        {
+            auto const m = get_num_rows();
+            auto const n = get_num_cols();
+            auto convolved = Matrix<T>(m, n);
+            convolved.zero();
+            for (int i = 1; i < m - 1; i++) {
+                for (int j = 1; j < n - 1; j++) {
+                    int values[8];
+                    int valuesLen = sizeof(values) / sizeof(values[0]);
+                    values[0] = data.at(i - 1).at(j - 1);
+                    values[1] = data.at(i - 1).at(j + 0);
+                    values[2] = data.at(i - 1).at(j + 1);
+
+                    values[3] = data.at(i).at(j - 1);
+                    values[4] = data.at(i).at(j + 1);
+
+                    values[5] = data.at(i + 1).at(j - 1);
+                    values[6] = data.at(i + 1).at(j + 0);
+                    values[7] = data.at(i + 1).at(j + 1);
+
+                    std::sort(values, values + valuesLen);
+                    auto median = 0.0;
+                    median  = values[3] + values[4] / 2.0;
+                    
+
+                    convolved.at(i, j) = median;
                 }
             }
             return convolved;
